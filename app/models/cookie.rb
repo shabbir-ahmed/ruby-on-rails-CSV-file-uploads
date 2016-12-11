4c8a16545709c593e38a1bbf8a989ab13ccaded3
Shabbir Ahmed <shabbirahmed149@gmail.com>
2016-12-11 18:29:02 +0600

    Model

--- app/models/cookie.rb
+++ app/models/cookie.rb
@@ -4,6 +4,7 @@ class Cookie < ApplicationRecord
     def self.import(file)
         CSV.foreach(file.path, headers: true) do |row|
             Cookie.create! row.to_hash
+			#=> If you need to update session ID then create functionality here
         end
     end
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -2,6 +2,7 @@ class History < ApplicationRecord
     def self.import(file)
         CSV.foreach(file.path, headers: true) do |row|
             History.create! row.to_hash
+			#=> If you need to update session ID then create functionality here
         end
     end
 end
